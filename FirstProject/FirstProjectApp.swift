d4426744db69ef45745ce1c8762925994d38fa76
AJ <alexanderjessediaz@gmail.com>
2020-11-10 15:17:35 -0700

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by AJ Diaz on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

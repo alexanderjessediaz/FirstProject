ac3f513e9a736b5952364202f6fc5a5a5077032d
AJ <alexanderjessediaz@gmail.com>
2021-01-22 14:14:48 -0700

    download design code assets and style main view

--- FirstProject/ContentView.swift
+++ FirstProject/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 4.0) {
+            Spacer()
+            HStack {
+                HStack {
+                    Spacer()
+                    Image("Illustration 1")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                Spacer()
+            }
+            Text("Placeholder").fontWeight(.bold)
+                .foregroundColor(.white)
+            Text("Placeholder").font(.footnote)
+                .foregroundColor(.white)
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
+
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.dark)
+            ContentView()
+                .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/))
+                .preferredColorScheme(.dark)
+        }
     }
 }
--- FirstProject/FirstProjectApp.swift
+++ FirstProject/FirstProjectApp.swift
@@ -15,3 +15,9 @@ struct FirstProjectApp: App {
         }
     }
 }
+
+struct FirstProjectApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}

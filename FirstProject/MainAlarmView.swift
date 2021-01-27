93e7e75a615bb123636c3bbe0c1a2981369dc511
AJ <alexanderjessediaz@gmail.com>
2021-01-27 16:22:33 -0700

    add ScrollView to MainAlarmView

--- FirstProject/MainAlarmView.swift
+++ FirstProject/MainAlarmView.swift
@@ -9,20 +9,30 @@ import SwiftUI
 
 struct MainAlarmView: View {
     @State var show = false
+    @Namespace var namespace
+    
     var body: some View{
         ZStack {
             AlarmItem()
+                .matchedGeometryEffect(id: "Card", in:
+                                        namespace, isSource: !show)
                 .frame(width: 335, height: 250)
-            VStack{
-                if show {
+            if show {
+                ScrollView {
                     AlarmItem()
-                        .transition(.move(edge:
-                                .trailing))
-                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                        
+                        .matchedGeometryEffect(id:
+                                                "Card", in:namespace)
+                        .frame(height: 300)
+                    VStack {
+                        ForEach(0 ..< 20) { item in
+                            HomeRow()
+                        }
+                    }
+                    .padding()
                 }
+                .transition(.opacity)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             }
-            
         }
         .onTapGesture {
             withAnimation {
@@ -30,7 +40,6 @@ struct MainAlarmView: View {
             }
             
         }
-//        .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
     }
 }
 

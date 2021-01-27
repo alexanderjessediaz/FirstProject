e6b10293a37debd6b625da24448e63c3cbd08fe6
AJ <alexanderjessediaz@gmail.com>
2021-01-27 11:56:13 -0700

    add animation, transitions and onTapGesture to MainAlarmView

--- FirstProject/MainAlarmView.swift
+++ FirstProject/MainAlarmView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct MainAlarmView: View {
+    @State var show = false
     var body: some View{
-        Text("")
+        ZStack {
+            AlarmItem()
+                .frame(width: 335, height: 250)
+            VStack{
+                if show {
+                    AlarmItem()
+                        .transition(.move(edge:
+                                .trailing))
+                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        
+                }
+            }
+            
+        }
+        .onTapGesture {
+            withAnimation {
+                show.toggle()
+            }
+            
+        }
+//        .animation(/*@START_MENU_TOKEN@*/.easeIn/*@END_MENU_TOKEN@*/)
     }
 }
 
